b929c967c39c1405c879bf49132d1e074ea4b2ff
Sizimon <siizmonsus@gmail.com>
2022-11-27 16:43:44 +0000

    FEAT: Add function that calculates tax deduction to generate a Net income after tax.                                     FIX: Add art to seperate code segments in order to improve readability

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import gspread 
+import gspread
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -14,24 +14,26 @@ SHEET = GSPREAD_CLIENT.open('SKI_PASS_SALES')
 
 def user_sales_input():
     '''
-    Ask the user to input the sales numbers. 
+    Ask the user to input the sales numbers.
     Use a loop to validate the string input by the user.
     Throw an error prompt if the data input is in incorrect format.
     '''
     while True:
-        print('------------------------------------------------------------')
+        print('============================================================\n')
         print('Welcome, I hope you have had a good day.\n')
         print('Please enter in the daily pass sales for today below.')
         print('Enter the data for our 5 locations seperated by commas.\n')
         print('Order: Are, Borgafjall, Hovden, Val de Saire, Zakopane.')
-        print('EG. 546, 259, 1203, 455, 756')
+        print('EG. 546, 259, 1203, 455, 756\n')
         print('------------------------------------------------------------\n')
-        
+
         user_daily_sales = input('Enter the daily pass sales for today:\n')
         print(f'The daily pass sales for today were: {user_daily_sales}.\n')
+        print('------------------------------------------------------------\n')
 
         user_weekly_sales = input('Enter the weekly pass sales for today:\n')
         print(f'The weekly pass sales for today were: {user_weekly_sales}.\n')
+        print('------------------------------------------------------------\n')
 
         daily_sales = user_daily_sales.split(',')
         weekly_sales = user_weekly_sales.split(',')
@@ -50,9 +52,9 @@ def user_sales_input():
                 else:
                     print('Invalid choice, valid choices are; Y or N.\n')
             break
-    
-    
-                    
+
+
+
 
 def data_validation(value1, value2):
     '''
@@ -69,9 +71,9 @@ def data_validation(value1, value2):
                 f'You need to provide 5 values, you provided {len(value1)} and {len(value2)}.'
             )
     except ValueError as e:
-            print(f'Invalid data: {e}, Please try again.\n')
-            return False
-    return True    
+        print(f'Invalid data: {e}, Please try again.\n')
+        return False
+    return True
 
 
 def update_worksheet(data, worksheet):
@@ -79,6 +81,7 @@ def update_worksheet(data, worksheet):
     Grabs the integers from the user input,
     and updates them into the designated worksheet.
     '''
+    print('------------------------------------------------------------\n')
     print(f'{worksheet} is now updating...')
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row([int(x) for x in data])
@@ -88,15 +91,16 @@ def update_worksheet(data, worksheet):
 def calculate_total_pass_sales(sum1, sum2):
     '''
     After input and confirmation from user, calculate total pass sales from day
-    by summing all the numbers input in daily row together, adding them to all  
+    by summing all the numbers input in daily row together, adding them to all
     numbers summed in weekly row.
     '''
+    print('------------------------------------------------------------\n')
     print('Calculating the total sales...\n')
-    
+
     daily_data = [int(num) for num in sum1]
     weekly_data = [int(num) for num in sum2]
     total_data = [num1 + num2 for num1, num2 in zip(daily_data, weekly_data)]
-    
+
     return total_data
 
 
@@ -108,12 +112,11 @@ def calculate_revenue_from_pass(sum1, sum2):
     Daily Pass is Priced at $25
     Weekly Pass is Priced at $125
     '''
-    gross = SHEET.worksheet('GROSS')
-
+    print('------------------------------------------------------------\n')
     proceed = input('Proceed with revenue calculations? (Y/N)\n')
     if proceed == 'Y':
         print('Calculating total revenue...\n')
-        
+
         daily_revenue = [int(x) * 25 for x in sum1]
         weekly_revenue = [int(x) * 125 for x in sum2]
         total_revenue = [num1 + num2 for num1, num2 in zip(daily_revenue, weekly_revenue)]
@@ -122,7 +125,22 @@ def calculate_revenue_from_pass(sum1, sum2):
         print('\nAborting revenue calculations.')
         main()
 
-    
+
+def calculate_net_from_pass(sum):
+    '''
+    Tell the user that the net will be calculated and ask them to commence the sequence,
+    the program will then take the calculated revenue from before and take off the sales tax
+    to produce a net.
+    '''
+    print('------------------------------------------------------------\n')
+    proceed = input('Proceed with the net revenue calculations? (Y/N)\n')
+    if proceed == 'Y':
+        print('Calculating net revenue...\n')
+        total_net = [int(x) * 67/100 for x in sum]
+        return total_net
+    elif proceed == 'N':
+        print('\nAborting net calculations.')
+        main()
 
 def main():
     '''
@@ -134,11 +152,12 @@ def main():
         update_worksheet(daily_sales, 'DAILY')
         weekly_sales = data[1]
         update_worksheet(weekly_sales, 'WEEKLY')
-        print('------------------------------------------------------------\n')
         total_sales = calculate_total_pass_sales(daily_sales, weekly_sales)
         update_worksheet(total_sales, 'TOTAL')
-        print('\n------------------------------------------------------------\n')
         revenue_data = calculate_revenue_from_pass(daily_sales, weekly_sales)
         update_worksheet(revenue_data, 'GROSS')
+        net_data = calculate_net_from_pass(revenue_data)
+        update_worksheet(net_data, 'NET')
+        print('============================================================')
 
 main()
